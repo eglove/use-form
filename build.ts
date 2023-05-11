446766111827a2566054d715a1150dcca0f6b0f6
Ethan Glover <hello@ethang.email>
2023-05-10 19:40:08 -0500

    init

---
+++ build.ts
@@ -0,0 +1,38 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import {execSync} from 'child_process';
+import {build as esbuild} from 'esbuild';
+import {globPlugin} from 'esbuild-plugin-glob';
+import {rimraf} from "rimraf";
+
+const build = async () => {
+    const root = __dirname;
+
+    await rimraf(path.join(root, 'dist')).catch();
+    execSync('tsc --project tsconfig.json');
+
+    await esbuild({
+        bundle: true,
+        entryPoints: ['src/*.ts'],
+        loader: {'.ts': 'ts'},
+        minify: true,
+        outdir: 'dist',
+        platform: 'browser',
+        plugins: [globPlugin()],
+        sourcemap: true,
+        target: 'esnext',
+    });
+
+    fs.copyFileSync(
+        path.join(root, 'package.json'),
+        path.join(root, 'dist', 'package.json'),
+    );
+};
+
+build()
+    .then(() => {
+        console.info('Complete!');
+    })
+    .catch(error => {
+        console.error(error);
+    });
